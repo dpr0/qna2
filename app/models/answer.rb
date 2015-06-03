745cf6aa07cbf2735b21a2791b13f31df970779e
dimarikpro <dimarik.pro@gmail.com>
2015-06-04 00:18:50 +0300

    created models and specs

---
+++ app/models/answer.rb
@@ -0,0 +1,8 @@
+class Answer < ActiveRecord::Base
+  belongs_to :question
+  belongs_to :user
+
+  validates :body,         presence: true, length: { minimum: 5, maximum: 1000 }
+  validates :question_id,  presence: true
+  validates :user_id,  presence: true
+end
---
+++ app/models/question.rb
@@ -0,0 +1,8 @@
+class Question < ActiveRecord::Base
+  has_many   :answers, dependent: :destroy
+  belongs_to :user
+
+  validates :title,   presence: true, length: { minimum: 5, maximum: 140 }
+  validates :body,    presence: true, length: { minimum: 5, maximum: 1000 }
+  validates :user_id, presence: true
+end
