8063eac46d9d4347289978a93fbc1fe8fdc4f928
dimarikpro <dimarik.pro@gmail.com>
2015-09-24 08:32:01 +0300

    l21 - add unicorn2

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.4.0'
 set :application, 'qna'
 set :repo_url, 'git@github.com:dimarikpro/qna2.git'
 set :deploy_to, '/home/deploy/qna'
-#set :deploy_user, 'deploy'
+set :deploy_user, 'deploy'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/private_pub.yml', 'config/private_pub_thin.yml', '.env') #, 'config/secrets.yml'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
